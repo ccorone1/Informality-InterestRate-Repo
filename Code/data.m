2cc6de064488d468675ecb10ae1d2b0aea2e76db
ccorone1 <cgaelcoronelr05@gmail.com>
2026-02-15 16:53:54 -0600

    Cleaning

--- Code/data.m
+++
@@ -1,8 +0,0 @@
-%Data Testing
-
-
-filename_test = "C:\Users\Carlos Coronel\Documents\Respaldo Memoria\leyva_research\data\clean\mensuales_2005.xlsx";
-data_test = readtable(filename_test);
-
-filename_test_tri = "C:\Users\Carlos Coronel\Documents\Respaldo Memoria\leyva_research\data\clean\trimestrales_primero.xlsx";
-data_test_tri = readtable(filename_test_tri);
